9c841d47ba62baf9405cbda9ef863246fa4ce6e9
Alex <contact@alexdecesare.com>
2021-09-17 14:22:02 -0400

    Added initial files
    Added the initial files

---
+++ src/edu/westga/cs3110/unicoder/model/CodePoint.java
@@ -0,0 +1,14 @@
+package edu.westga.cs3110.unicoder.model;
+
+/**
+ * Gets the code point for given UTF encodings
+ * 
+ * @author Alex DeCesare
+ * @version 17-September-2021
+ */
+
+public class CodePoint {
+	
+
+
+}
